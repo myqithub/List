120c888267aaaf9bad9f9d02334b5ac030e92936
veysel dogan <veyselgithub@gmail.com>
2018-08-30 09:59:28 +0300

    List

---
+++ src/IntListVer1.java
@@ -0,0 +1,35 @@
+ /* @author: Veysel Dogan
+ * Date: 30-Aug-2018 
+ * Console Application: intlistver
+ * Main Class
+ */
+public class IntListVer1 {
+    // class constant for default size
+    private static final int DEFAULT_CAP = 10;
+    
+    //instance variables
+    private int[] iValues;
+    private int iSize;
+    
+    /**
+     * Default constructor. Creates an empty list.
+     */
+    public IntListVer1(){
+        //redirect to single int constructor
+        this(DEFAULT_CAP);
+        //other statments could go here.
+    }
+
+    /**
+     * Constructor to allow user of class to specify 
+     * initial capacity in case they intend to add a lot
+     * of elements to new list. Creates an empty list.
+     * @param initialCap > 0
+     */    
+    public IntListVer1(int initialCap) {
+        assert initialCap > 0 : "Violation of precondition. IntListVer1(int initialCap):"
+            + "initialCap must be greater than 0. Value of initialCap: " + initialCap;
+        iValues = new int[initialCap];
+        iSize = 0;
+    }
+}
